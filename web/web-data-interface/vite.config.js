edec2e2846a42d9ea40dbd900c2b632cd794f2d5
unknown <oaugusto796@gmail.com>
2024-11-18 14:12:21 -0300

    Alter app for CORS adjust

--- web/web-data-interface/vite.config.js
+++ web/web-data-interface/vite.config.js
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    proxy: {
-      '/api': {
-        target: 'https://iomt-data-api-7752107602.us-central1.run.app',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
+    port: 3000,
+    host: true,
+  },
 });
