ad4e7c10e094a16196c0e0280482a183f3292161
Andre Sichelero <andre.gasolisichelero@rwdi.com>
2024-11-25 22:54:14 -0300

    formatting

--- sensores/util.py
+++ sensores/util.py
@@ -1,8 +1,9 @@
-import random 
+import random
 import psycopg2
 import os
 from dotenv import load_dotenv
 import requests
+
 
 class User:
     def __init__(self, id, type, value1, value2, dateTime):
@@ -13,33 +14,41 @@ class User:
         self.dateTime = dateTime
 
     def to_dict(self):
-        return {"userid": self.id, "type": self.type, "value1":self.value1, "value2":self.value2, "inhouse":True}
+        return {
+            "userid": self.id,
+            "type": self.type,
+            "value1": self.value1,
+            "value2": self.value2,
+            "inhouse": True,
+        }
+
 
 class Generator:
     def generate(self, active_users):
         for i in active_users:
-            normal = True if random.randint(1,10) <=8 else False
-            if(normal):
+            normal = True if random.randint(1, 10) <= 8 else False
+            if normal:
                 if i.type == 1:
                     i.value1 = random.randint(110, 129)
-                    i.value2 =  random.randint(70, 84)
+                    i.value2 = random.randint(70, 84)
                 elif i.type == 2:
                     i.value1 = random.randint(95, 100)
                     i.value2 = random.randint(50, 100)
                 elif i.type == 3:
-                    i.value1 = round(random.uniform(36.0, 37.5),2)
+                    i.value1 = round(random.uniform(36.0, 37.5), 2)
                     i.value2 = 0
             else:
                 if i.type == 1:
                     i.value1 = random.randint(0, 300)
-                    i.value2 =  random.randint(0, 300)
+                    i.value2 = random.randint(0, 300)
                 elif i.type == 2:
                     i.value1 = random.randint(0, 100)
                     i.value2 = random.randint(0, 200)
                 elif i.type == 3:
-                    i.value1 = round(random.uniform(30.0, 45.0),2)
+                    i.value1 = round(random.uniform(30.0, 45.0), 2)
                     i.value2 = 0
         return active_users
+
 
 class DataBase:
     def __init__(self):
@@ -47,9 +56,7 @@ class DataBase:
 
     def returnActiveUsers(self):
         url = os.getenv("DATABASE_URL")
-        conn = psycopg2.connect(
-            url
-        )
+        conn = psycopg2.connect(url)
         cursor = conn.cursor()
         users = []
         try:
@@ -68,23 +75,34 @@ class DataBase:
                     type_id = user_type[0]
 
                     # Busca o dado coletado mais recente para cada tipo e usuário
-                    cursor.execute("""
+                    cursor.execute(
+                        """
                         SELECT value1, value2, datetime 
                         FROM collected_data 
                         WHERE userid = %s AND type = %s 
                         ORDER BY dateTime DESC 
                         LIMIT 1;
-                    """, (user_id, type_id))
-                    
+                    """,
+                        (user_id, type_id),
+                    )
+
                     recent_data = cursor.fetchone()
 
                     # Só adiciona o usuário se houverem dados
                     if recent_data:
                         value1, value2, dateTime = recent_data
-                        user_obj = User(id=user_id, type=type_id, value1=value1, value2=value2, dateTime=dateTime)
+                        user_obj = User(
+                            id=user_id,
+                            type=type_id,
+                            value1=value1,
+                            value2=value2,
+                            dateTime=dateTime,
+                        )
                         users.append(user_obj)
                     else:
-                        user_obj = User(id=user_id, type=type_id, value1=0, value2=0, dateTime=None)
+                        user_obj = User(
+                            id=user_id, type=type_id, value1=0, value2=0, dateTime=None
+                        )
                         users.append(user_obj)
         except Exception as e:
             print(f"Erro ao realizar consultas: {e}")
@@ -92,29 +110,36 @@ class DataBase:
             cursor.close()
             conn.close()
         return users
-    
+
+
 class postToRest:
     api_url = "https://iomt-data-api-7752107602.us-central1.run.app/collected-data/"
     auth_url = "https://iomt-data-api-7752107602.us-central1.run.app/token"
+
     def __init__(self):
         load_dotenv()
         admin_email = os.getenv("ADMIN_EMAIL")
         admin_password = os.getenv("ADMIN_PASSWORD")
-        data={"username":admin_email, "password":admin_password}
+        data = {"username": admin_email, "password": admin_password}
         response = requests.post(self.auth_url, data=data)
         if response.status_code == 200:
             self.token = response.json().get("access_token")
         else:
             self.token = None
-    
+
     def post_users(self, users):
-        if(self.token == None):
+        if self.token == None:
             return
-        headers = {"Authorization": f"Bearer {self.token}", "Content-Type": "application/json"}
+        headers = {
+            "Authorization": f"Bearer {self.token}",
+            "Content-Type": "application/json",
+        }
         for user in users:
             data = user.to_dict()
             response = requests.post(self.api_url, json=data, headers=headers)
             if response.status_code == 200:
                 print("Usuário enviado com sucesso!")
             else:
-                print(f"Erro ao enviar usuário: {response.status_code} - {response.text}")
+                print(
+                    f"Erro ao enviar usuário: {response.status_code} - {response.text}"
+                )
